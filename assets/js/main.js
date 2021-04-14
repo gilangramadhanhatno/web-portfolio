148f538c22215832caaa70b07add18cea06a19bc
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-04-14 23:01:50 +0700

    menambahkan hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,33 @@
-$(document).on("click", ".mobile-nav-toggle", function (e) {
-  $("body").toggleClass("mobile-nav-active");
-  $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
-});
+!(function ($) {
+  "use strict";
+
+  // Hero typed
+  if ($(".typed").length) {
+    var typed_strings = $(".typed").data("typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+
+  $(document).on("click", ".mobile-nav-toggle", function (e) {
+    $("body").toggleClass("mobile-nav-active");
+    $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out-back",
+      once: true,
+    });
+  }
+  $(window).on("load", function () {
+    aos_init();
+  });
+})(jQuery);
