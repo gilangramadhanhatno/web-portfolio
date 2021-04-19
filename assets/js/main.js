13217e65ab03810e5feea1e56fcf5acb1671dc69
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-04-19 14:20:14 +0700

    menambahkan skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,8 +32,20 @@
   });
 })(jQuery);
 
-// jQuery counterUp
+// jQuery counterUp / facts scetion
 $('[data-toggle="counter-up"]').counterUp({
   delay: 10,
   time: 1000,
 });
+
+// Skills section
+$(".skills-content").waypoint(
+  function () {
+    $(".progress .progress-bar").each(function () {
+      $(this).css("width", $(this).attr("aria-valuenow") + "%");
+    });
+  },
+  {
+    offset: "80%",
+  }
+);
