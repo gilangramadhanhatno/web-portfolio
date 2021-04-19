c56d6a9fc941109d9d4d89337d4cb787ee6de6c0
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-04-20 04:04:21 +0700

    menambahkan back-to-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,104 @@
     $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
   });
 
+  // jQuery counterUp / facts section
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+  // Skills section
+  $(".skills-content").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    {
+      offset: "80%",
+    }
+  );
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+    },
+  });
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on("click", ".nav-menu a, .scrollto", function (e) {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+        var scrollto = target.offset().top;
+
+        $("html, body").animate(
+          {
+            scrollTop: scrollto,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+
+        if ($(this).parents(".nav-menu, .mobile-nav").length) {
+          $(".nav-menu .active, .mobile-nav .active").removeClass("active");
+          $(this).closest("li").addClass("active");
+        }
+
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function () {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $("html, body").animate(
+          {
+            scrollTop: scrollto,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+      }
+    }
+  });
+
+  $(document).on("click", ".mobile-nav-toggle", function (e) {
+    $("body").toggleClass("mobile-nav-active");
+    $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+  });
+
+  $(document).click(function (e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($("body").hasClass("mobile-nav-active")) {
+        $("body").removeClass("mobile-nav-active");
+        $(".mobile-nav-toggle i").toggleClass("icofont-navigation-menu icofont-close");
+      }
+    }
+  });
+
   // Init AOS
   function aos_init() {
     AOS.init({
@@ -31,39 +129,3 @@
     aos_init();
   });
 })(jQuery);
-
-// jQuery counterUp / facts scetion
-$('[data-toggle="counter-up"]').counterUp({
-  delay: 10,
-  time: 1000,
-});
-
-// Skills section
-$(".skills-content").waypoint(
-  function () {
-    $(".progress .progress-bar").each(function () {
-      $(this).css("width", $(this).attr("aria-valuenow") + "%");
-    });
-  },
-  {
-    offset: "80%",
-  }
-);
-
-// Testimonials carousel (uses the Owl Carousel library)
-$(".testimonials-carousel").owlCarousel({
-  autoplay: true,
-  dots: true,
-  loop: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    768: {
-      items: 2,
-    },
-    900: {
-      items: 3,
-    },
-  },
-});
