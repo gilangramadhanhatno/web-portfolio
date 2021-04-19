f81e50cf99193841db3aca86f561eb92f65700c6
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-04-20 00:08:00 +0700

    menambahkan testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,21 @@ $(".skills-content").waypoint(
     offset: "80%",
   }
 );
+
+// Testimonials carousel (uses the Owl Carousel library)
+$(".testimonials-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    900: {
+      items: 3,
+    },
+  },
+});
